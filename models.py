fix transition matrix normalization in models.py

models.py
@@ -50,7 +50,7 @@ class HMM(torch.nn.Module):
 
 	def sample(self, T=10):
 		state_priors = torch.nn.functional.softmax(self.unnormalized_state_priors, dim=0)
-		transition_matrix = torch.nn.functional.softmax(self.transition_model.unnormalized_transition_matrix, dim=1)
+		transition_matrix = torch.nn.functional.softmax(self.transition_model.unnormalized_transition_matrix, dim=0)
 		emission_matrix = torch.nn.functional.softmax(self.emission_model.unnormalized_emission_matrix, dim=1)
 
 		# sample initial state
@@ -171,7 +171,7 @@ class TransitionModel(torch.nn.Module):
 		Multiply previous timestep's alphas by transition matrix (in log domain)
 		"""
 		# Each col needs to add up to 1 (in probability domain)
-		log_transition_matrix = torch.nn.functional.log_softmax(self.unnormalized_transition_matrix, dim=1)
+		log_transition_matrix = torch.nn.functional.log_softmax(self.unnormalized_transition_matrix, dim=0)
 
 		# Matrix multiplication in the log domain
 		#out = genbmm.logbmm(log_alpha.unsqueeze(0).contiguous(), transition_matrix.unsqueeze(0).contiguous())[0]